d9fa62d23ac471206cb947a6e011c317456c61a9
ice1000 <ice1000@sbrella.com>
2018-03-20 12:24:30 +0800

    [ #5 ] Closes #5

--- src/org/ziglang/editing/zig-editing.kt
+++ src/org/ziglang/editing/zig-editing.kt
@@ -1,7 +1,10 @@
 package org.ziglang.editing
 
-import com.intellij.lang.Commenter
+import com.intellij.lang.*
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
 import org.ziglang.ZIG_COMMENT_START
+import org.ziglang.psi.ZigTypes
 
 class ZigCommenter : Commenter {
 	override fun getCommentedBlockCommentPrefix(): String? = blockCommentPrefix
@@ -11,4 +14,16 @@ class ZigCommenter : Commenter {
 	override fun getLineCommentPrefix() = ZIG_COMMENT_START
 }
 
+class ZigBraceMatcher : PairedBraceMatcher {
+	private companion object PairHolder {
+		private val PAIRS = arrayOf(
+				BracePair(ZigTypes.LEFT_BRACE, ZigTypes.RIGHT_BRACE, false),
+				BracePair(ZigTypes.LEFT_BRACKET, ZigTypes.RIGHT_BRACKET, false),
+				BracePair(ZigTypes.LEFT_PAREN, ZigTypes.RIGHT_PAREN, false)
+		)
+	}
 
+	override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int) = openingBraceOffset
+	override fun getPairs() = PAIRS
+	override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+}
