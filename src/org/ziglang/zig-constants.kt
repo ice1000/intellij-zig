ffc316be512c64b1d35edb3b93ffb8b55bb7ab65
ice1000 <ice1000@sbrella.com>
2018-03-20 12:20:25 +0800

    [ #5 ] Add commenter

--- src/org/ziglang/zig-constants.kt
+++ src/org/ziglang/zig-constants.kt
@@ -6,3 +6,6 @@ import org.jetbrains.annotations.NonNls
 @NonNls const val ZIG_EXTENSION = "zig"
 
 @NonNls const val ZIG_WEBSITE = "https://ziglang.org"
+
+@NonNls const val ZIG_COMMENT_START = "//"
+
