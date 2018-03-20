859a1c36cda0aa355941c3c4cdb6baa7a62bba0e
LimbolRain <limbolrain@gmail.com>
2018-03-21 00:12:38 +0800

    [ #6 ] try it

--- src/org/ziglang/project/ui/ZigProjectGeneratorPeer.java
+++ src/org/ziglang/project/ui/ZigProjectGeneratorPeer.java
@@ -1,5 +1,6 @@
 package org.ziglang.project.ui;
 
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.platform.ProjectGeneratorPeer;
 import org.jetbrains.annotations.NotNull;
 import org.ziglang.project.ZigSettings;
@@ -8,4 +9,6 @@ import javax.swing.*;
 
 public abstract class ZigProjectGeneratorPeer implements ProjectGeneratorPeer<ZigSettings> {
 	protected @NotNull JPanel mainPanel;
+	protected @NotNull
+	TextFieldWithBrowseButton executablePath;
 }
--- src/org/ziglang/project/ui/ui-impl.kt
+++ src/org/ziglang/project/ui/ui-impl.kt
@@ -1,12 +1,20 @@
 package org.ziglang.project.ui
 
 import com.intellij.ide.util.projectWizard.SettingsStep
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.ui.TextBrowseFolderListener
 import com.intellij.openapi.ui.ValidationInfo
+import org.ziglang.ZigBundle
 import org.ziglang.project.ZigSettings
 
 
 class ZigProjectGeneratorPeerImpl : ZigProjectGeneratorPeer() {
 	private val settings = ZigSettings()
+
+	init {
+		executablePath.addBrowseFolderListener(TextBrowseFolderListener(FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor()))
+	}
+
 	@Suppress("OverridingDeprecatedMember")
 	override fun addSettingsStateListener(
 			@Suppress("DEPRECATION") listener: com.intellij.platform.WebProjectGenerator.SettingsStateListener) = Unit
@@ -18,7 +26,13 @@ class ZigProjectGeneratorPeerImpl : ZigProjectGeneratorPeer() {
 	}
 
 	override fun validate(): ValidationInfo? {
-		return null
+		val path = executablePath.text
+		return if (true) {    //TODO check the executable is valid
+			settings.exePath = path
+			null
+		} else {
+			ValidationInfo(ZigBundle.message("zig.project.invalid"))
+		}
 	}
 
 	override fun isBackgroundJobRunning() = false
