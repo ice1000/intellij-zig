6dd31243ec6e9b8d9f6e4a87308af9d70a13126b
ice1000 <ice1000kotlin@foxmail.com>
2018-03-22 02:33:36 +0800

    [ #8 ] More

--- src/org/ziglang/execution/ui-impl.kt
+++ src/org/ziglang/execution/ui-impl.kt
@@ -7,8 +7,10 @@ class ZigRunConfigurationEditorImpl(configuration: ZigRunConfiguration) : ZigRun
 
 	override fun createEditor() = mainPanel
 	override fun resetEditorFrom(configuration: ZigRunConfiguration) {
+		// TODO swing 泛型编程
 	}
 
 	override fun applyEditorTo(configuration: ZigRunConfiguration) {
+		// TODO swing 泛型编程
 	}
 }
