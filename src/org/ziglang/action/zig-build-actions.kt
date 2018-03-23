9037804cf9ca309c99620085f7fe4c2b524c5c8e
ice1000 <ice1000kotlin@foxmail.com>
2018-03-24 03:06:32 +0800

    [ bug ] Fix

--- src/org/ziglang/action/zig-build-actions.kt
+++ src/org/ziglang/action/zig-build-actions.kt
@@ -1,27 +1,21 @@
 package org.ziglang.action
 
 import com.google.common.io.Files
-import com.intellij.compiler.actions.CompileActionBase
 import com.intellij.execution.*
 import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.project.Project
 import org.ziglang.ZigBundle
 import org.ziglang.execution.ZigRunConfiguration
 import org.ziglang.execution.ZigRunConfigurationType
 import org.ziglang.trimPath
 
-class ZigBuildAction : CompileActionBase() {
-	init {
-		templatePresentation.run {
-			text = ZigBundle.message("zig.actions.build.title")
-			description = ZigBundle.message("zig.actions.build.description")
-		}
-	}
-
-	override fun doAction(dataContext: DataContext, project: Project) {
-		val file = CommonDataKeys.VIRTUAL_FILE.getData(dataContext) ?: return
+class ZigBuildAction : ZigAction(
+		ZigBundle.message("zig.actions.build.title"),
+		ZigBundle.message("zig.actions.build.description")) {
+	override fun actionPerformed(e: AnActionEvent) {
+		val project = e.project ?: return
+		val file = CommonDataKeys.VIRTUAL_FILE.getData(e.dataContext) ?: return
 		val path = file.path.trimPath()
 		val executor = ExecutorRegistry.getInstance().getExecutorById(DefaultRunExecutor.EXECUTOR_ID)
 		val configuration = RunManager
