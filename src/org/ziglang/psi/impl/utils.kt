02bc3f022684f279daaeeb0e98a86dcc65ce8239
ice1000 <ice1000kotlin@foxmail.com>
2018-03-24 03:13:22 +0800

    [ annotator ] Function name fix

---
+++ src/org/ziglang/psi/impl/utils.kt
@@ -0,0 +1,24 @@
+package org.ziglang.psi.impl
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.IElementType
+
+fun PsiElement.prevSiblingTypeIgnoring(type: IElementType, vararg types: IElementType): PsiElement? {
+	var next: PsiElement? = prevSibling
+	while (true) {
+		val localNext = next ?: return null
+		next = localNext.prevSibling
+		return if (types.any { localNext.node.elementType == it }) continue
+		else localNext.takeIf { it.node.elementType == type }
+	}
+}
+
+inline fun <reified Psi : PsiElement> PsiElement.prevSiblingIgnoring(vararg types: IElementType): Psi? {
+	var next: PsiElement? = prevSibling
+	while (true) {
+		val localNext = next ?: return null
+		next = localNext.prevSibling
+		return if (types.any { localNext.node.elementType == it }) continue
+		else localNext as? Psi
+	}
+}
--- src/org/ziglang/psi/impl/zig-mixins.kt
+++ src/org/ziglang/psi/impl/zig-mixins.kt
@@ -2,10 +2,9 @@ package org.ziglang.psi.impl
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.TokenType
 import org.ziglang.ZigTokenType
-import org.ziglang.orFalse
 import org.ziglang.psi.*
 
 interface IZigSymbol : PsiNameIdentifierOwner {
@@ -14,7 +13,10 @@ interface IZigSymbol : PsiNameIdentifierOwner {
 
 abstract class ZigSymbolMixin(node: ASTNode) : ASTWrapperPsiElement(node), ZigSymbol {
 	override val isFunctionName: Boolean
-		get() = parent is ZigFnProto && prevSibling?.run { node.elementType == ZigTypes.FN_KEYWORD }.orFalse()
+		get() = parent is ZigFnProto && prevSiblingTypeIgnoring(
+				ZigTypes.FN_KEYWORD,
+				TokenType.WHITE_SPACE,
+				ZigTokenType.LINE_COMMENT) != null
 
 	override fun getNameIdentifier() = this
 	override fun setName(name: String) = replace(ZigTokenType.fromText(name, project))
