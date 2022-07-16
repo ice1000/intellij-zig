9909cfe9f21d24d0fab0da6b8fbe245ec99bf907
linux_china <libing.chen@gmail.com>
2022-07-16 14:36:54 -0700

    chore: add run icon

--- src/main/java/icons/ZigIcons.java
+++ src/main/java/icons/ZigIcons.java
@@ -11,13 +11,15 @@ import javax.swing.*;
  * @author ice1000, HoshinoTended
  */
 public interface ZigIcons {
-	@NotNull Icon ZIG_BIG_ICON = IconLoader.getIcon("/icons/zig_icon.png");
-	@NotNull Icon ZIG_ADD_SDK_ICON = IconLoader.getIcon("/icons/zig_sdk.png");
-	@NotNull Icon ZIG_WEBSITE_ICON = IconLoader.getIcon("/icons/zig.png");
-	// @NotNull Icon ZIG_RESOURCE_ICON = IconLoader.getIcon("/icons/zig_resource.png");
-	@NotNull Icon ZIG_FILE = IconLoader.getIcon("/icons/zig_file.png");
-	@NotNull Icon ZIG_VAR = IconLoader.getIcon("/icons/zig_variable.png");
-	@NotNull Icon ZIG_FUN = IconLoader.getIcon("/icons/zig_function.png");
+    @NotNull Icon ZIG_BIG_ICON = IconLoader.getIcon("/icons/zig_icon.png");
+    @NotNull Icon ZIG_ADD_SDK_ICON = IconLoader.getIcon("/icons/zig_sdk.png");
+    @NotNull Icon ZIG_WEBSITE_ICON = IconLoader.getIcon("/icons/zig.png");
+    // @NotNull Icon ZIG_RESOURCE_ICON = IconLoader.getIcon("/icons/zig_resource.png");
+    @NotNull Icon ZIG_FILE = IconLoader.getIcon("/icons/zig_file.png");
+    @NotNull Icon ZIG_VAR = IconLoader.getIcon("/icons/zig_variable.png");
+    @NotNull Icon ZIG_FUN = IconLoader.getIcon("/icons/zig_function.png");
 
-	@NotNull Icon ZIG_TEST_MARK = IconLoader.getIcon("/icons/zig_test_mark.svg");
+    @NotNull Icon ZIG_TEST_MARK = IconLoader.getIcon("/icons/zig_test_mark.svg");
+    @NotNull Icon RUN_ICON = IconLoader.getIcon("/runConfigurations/testState/run.svg");
+
 }
