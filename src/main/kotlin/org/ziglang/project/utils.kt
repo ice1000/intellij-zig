f074beed2cedd51258e8773a7caee161ccb91b3d
linux_china <libing.chen@gmail.com>
2022-07-16 11:17:40 -0700

    chore: adjust sdk home validation

--- src/main/kotlin/org/ziglang/project/utils.kt
+++ src/main/kotlin/org/ziglang/project/utils.kt
@@ -10,7 +10,6 @@ import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.ComboboxWithBrowseButton
 import com.intellij.ui.components.labels.LinkLabel
-import com.intellij.ui.components.labels.LinkListener
 import org.ziglang.ZigBundle
 import org.ziglang.executeCommand
 import java.nio.file.Files
@@ -18,53 +17,56 @@ import java.nio.file.Paths
 import javax.swing.JComboBox
 
 val zigPath: String by lazy {
-	PathEnvironmentVariableUtil.findInPath("zig")?.absolutePath
-			?: when {
-				SystemInfo.isWindows -> "C://Program Files/"
-				SystemInfo.isMac -> "/usr/local/bin"
-				else -> "/usr/bin/zig"
-			}
+    PathEnvironmentVariableUtil.findInPath("zig")?.absolutePath
+        ?: when {
+            SystemInfo.isWindows -> "C://Program Files/"
+            SystemInfo.isMac -> "/usr/local/bin"
+            else -> "/usr/bin/zig"
+        }
 }
 
 fun versionOf(path: String) = executeCommand(arrayOf(path, "version"))
-		.first
-		.firstOrNull()
-		?: ZigBundle.message("zig.version.unknown")
+    .first
+    .firstOrNull()
+    ?: ZigBundle.message("zig.version.unknown")
 
 fun findOrCreate(baseDir: VirtualFile, dir: String, module: Module) =
-		baseDir.findChild(dir) ?: baseDir.createChildDirectory(module, dir)
+    baseDir.findChild(dir) ?: baseDir.createChildDirectory(module, dir)
 
 fun validateZigExe(exePath: String) = Files.exists(Paths.get(exePath))
 
 // https://github.com/zig-lang/zig/blob/7350181a4a778f9d03186e5123beffdf80f58606/src/main.cpp#L140-L173
 fun validateZigLib(installPath: String) = sequenceOf("zig.zig", "index.zig", "std.zig").any { file ->
-	Files.exists(Paths.get(installPath, "lib", "zig", "std", file))
+    Files.exists(Paths.get(installPath, "lib", "zig", "std", file))
             || Files.exists(Paths.get(installPath, "lib", "std", file))
 }
 
-fun validateZigSDK(sdkHome: String) = Files.exists(Paths.get(sdkHome, "bin", "zig")) ||
-		Files.exists(Paths.get(sdkHome, "bin", "zig.exe"))
+fun validateZigSDK(sdkHome: String) = Files.exists(Paths.get(sdkHome, "bin", "zig"))
+        || Files.exists(Paths.get(sdkHome, "bin", "zig.exe"))
+        || Files.exists(Paths.get(sdkHome, "zig"))
+        || Files.exists(Paths.get(sdkHome, "zig.exe"))
 
 fun LinkLabel<Any>.asLink() {
-	setListener({ _, _ ->
-		BrowserLauncher.instance.browse(text)
-	}, null)
+    setListener({ _, _ ->
+        BrowserLauncher.instance.browse(text)
+    }, null)
 }
 
 inline fun initExeComboBox(
-		zigExeField: ComboboxWithBrowseButton,
-		project: Project? = null,
-		crossinline addListener: (ComboboxWithBrowseButton) -> Unit = {}) {
-	zigExeField.addBrowseFolderListener(ZigBundle.message("zig.messages.run.select-compiler"),
-			ZigBundle.message("zig.messages.run.select-compiler.description"),
-			project,
-			FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor(),
-			object : TextComponentAccessor<JComboBox<Any>> {
-				override fun getText(component: JComboBox<Any>) = component.selectedItem as? String ?: ""
-				override fun setText(component: JComboBox<Any>, text: String) {
-					component.addItem(text)
-					addListener(zigExeField)
-				}
-			})
-	zigGlobalSettings.knownZigExes.forEach(zigExeField.comboBox::addItem)
+    zigExeField: ComboboxWithBrowseButton,
+    project: Project? = null,
+    crossinline addListener: (ComboboxWithBrowseButton) -> Unit = {}
+) {
+    zigExeField.addBrowseFolderListener(ZigBundle.message("zig.messages.run.select-compiler"),
+        ZigBundle.message("zig.messages.run.select-compiler.description"),
+        project,
+        FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor(),
+        object : TextComponentAccessor<JComboBox<Any>> {
+            override fun getText(component: JComboBox<Any>) = component.selectedItem as? String ?: ""
+            override fun setText(component: JComboBox<Any>, text: String) {
+                component.addItem(text)
+                addListener(zigExeField)
+            }
+        })
+    zigGlobalSettings.knownZigExes.forEach(zigExeField.comboBox::addItem)
 }
