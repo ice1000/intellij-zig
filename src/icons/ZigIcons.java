fea6c724f7b7534b80d418966f4338999afa7d6f
Anatol Pomozov <anatol.pomozov@gmail.com>
2022-10-25 09:09:05 -0700

    Use IconLoader.getIcon(name,class) function instead of the deprecated version

--- src/icons/ZigIcons.java
+++ src/icons/ZigIcons.java
@@ -11,11 +11,11 @@ import javax.swing.*;
  * @author ice1000, HoshinoTended
  */
 public interface ZigIcons {
-	@NotNull Icon ZIG_BIG_ICON = IconLoader.getIcon("/icons/zig_icon.png");
-	@NotNull Icon ZIG_ADD_SDK_ICON = IconLoader.getIcon("/icons/zig_sdk.png");
-	@NotNull Icon ZIG_WEBSITE_ICON = IconLoader.getIcon("/icons/zig.png");
-	// @NotNull Icon ZIG_RESOURCE_ICON = IconLoader.getIcon("/icons/zig_resource.png");
-	@NotNull Icon ZIG_FILE = IconLoader.getIcon("/icons/zig_file.png");
-	@NotNull Icon ZIG_VAR = IconLoader.getIcon("/icons/zig_variable.png");
-	@NotNull Icon ZIG_FUN = IconLoader.getIcon("/icons/zig_function.png");
+	@NotNull Icon ZIG_BIG_ICON = IconLoader.getIcon("/icons/zig_icon.png", ZigIcons.class);
+	@NotNull Icon ZIG_ADD_SDK_ICON = IconLoader.getIcon("/icons/zig_sdk.png", ZigIcons.class);
+	@NotNull Icon ZIG_WEBSITE_ICON = IconLoader.getIcon("/icons/zig.png", ZigIcons.class);
+	// @NotNull Icon ZIG_RESOURCE_ICON = IconLoader.getIcon("/icons/zig_resource.png", ZigIcons.class);
+	@NotNull Icon ZIG_FILE = IconLoader.getIcon("/icons/zig_file.png", ZigIcons.class);
+	@NotNull Icon ZIG_VAR = IconLoader.getIcon("/icons/zig_variable.png", ZigIcons.class);
+	@NotNull Icon ZIG_FUN = IconLoader.getIcon("/icons/zig_function.png", ZigIcons.class);
 }
